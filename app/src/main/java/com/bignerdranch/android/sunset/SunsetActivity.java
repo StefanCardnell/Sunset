d49d291746c8294691f8d948ffc6111a04747e67
StefanCardnell <raging_rush@hotmail.co.uk>
2020-07-04 12:22:03 +0100

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/sunset/SunsetActivity.java
@@ -0,0 +1,11 @@
+package com.bignerdranch.android.sunset;
+
+import androidx.fragment.app.Fragment;
+
+public class SunsetActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return SunsetFragment.newInstance();
+    }
+}
